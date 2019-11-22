986fbc91f7949e6eedf1badcbf47683686ede127
Saijin-Naib <19295950+Saijin-Naib@users.noreply.github.com>
2019-11-22 10:10:14 -0500

    Update geohash32.py
    Add an empty array, append X/Y (Lon/Lat) to the array, alias mz2geohash.encode, and try to return a value. Also, adjusted value1 to value to match another working Field Calculator Expression.

--- geohash32.py
+++ geohash32.py
@@ -3,9 +3,12 @@ from qgis.gui import *
 import mz2geohash
 
 @qgsfunction(args='auto', group='Custom')
-def geohash32(value1,feature,parent):
+def geohash32(value,feature,parent):
     ft_centroid = centroid($geometry)
     ft_lon = x(transform(ft_centroid),layer_property(@layer_id,'crs'),'EPSG:4326'))
     ft_lat = y(transform(ft_centroid),layer_property(@layer_id,'crs'),'EPSG:4326'))
-    ft_tup = (ft_lon,ft_lat)
-    return mz2geohash.encode(ft_tup)
+    ft_array = []
+    ft_array.append(ft_lon)
+    ft_array.append(ft_lat)
+    geohash32 = mz2geohash.encode
+    return geohash32(ft_array)
