ca406beee7c59155fe732b917f54863daacb835c
Saijin-Naib <19295950+Saijin-Naib@users.noreply.github.com>
2019-11-22 09:48:55 -0500

    Update geohash32.py
    Verified that I have the variables correctly mapped between Lat/Lon and X/Y as per nmtoken; removed empty initialization of tuple as per Gabriel De Luca and ThingumaBob.

--- geohash32.py
+++ geohash32.py
@@ -4,9 +4,8 @@ import mz2geohash
 
 @qgsfunction(args='auto', group='Custom')
 def geohash32(value1,feature,parent):
-    ft_tup = ()
     ft_centroid = centroid($geometry)
-    ft_lon = x(transform(ft_centroid,layer_property(@layer_id,'crs'),'EPSG:4326'))
-    ft_lat = y(transform(ft_centroid,layer_property(@layer_id,'crs'),'EPSG:4326'))
-    ft_tup = array(ft_lon,ft_lat)
+    ft_lon = x(transform(ft_centroid),layer_property(@layer_id,'crs'),'EPSG:4326'))
+    ft_lat = y(transform(ft_centroid),layer_property(@layer_id,'crs'),'EPSG:4326'))
+    ft_tup = (ft_lon,ft_lat)
     return mz2geohash.encode(ft_tup)
