16d61365c24ca230164b4249d9fea43f48f1e075
Saijin-Naib <19295950+Saijin-Naib@users.noreply.github.com>
2019-11-26 13:24:45 -0500

    Update geohash32.py
    Adjust code to fix error below:
      File "", line 8
        ft_lon = x(transform(ft_centroid),layer_property(@layer_id,'crs'),'EPSG:4326'))
                                                         ^
    SyntaxError: invalid syntax

--- geohash32.py
+++ geohash32.py
@@ -5,8 +5,8 @@ import mz2geohash
 @qgsfunction(args='auto', group='Custom')
 def geohash32(value,feature,parent):
     ft_centroid = centroid(geometry)
-    ft_lon = x(transform(ft_centroid),layer_property(@layer_id,'crs'),'EPSG:4326'))
-    ft_lat = y(transform(ft_centroid),layer_property(@layer_id,'crs'),'EPSG:4326'))
+    ft_lon = x(transform(ft_centroid),layer_property(layer_id,'crs'),'EPSG:4326'))
+    ft_lat = y(transform(ft_centroid),layer_property(layer_id,'crs'),'EPSG:4326'))
     ft_array = []
     ft_array.append(ft_lon)
     ft_array.append(ft_lat)
